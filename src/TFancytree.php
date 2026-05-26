27e1807370498318df3d1aa5bc51103c952f5a18
Marcelo Barreto Nees <marcelo.linux@gmail.com>
2026-05-26 13:28:40 -0300

    Updt: Reestruturação de arquivos

--- src/TFancytree.php
+++ src/TFancytree.php
@@ -6,6 +6,7 @@ use Adianti\Widget\Base\TElement;
 use Adianti\Widget\Base\TScript;
 use Adianti\Control\TAction;
 use Adianti\Core\AdiantiCoreTranslator;
+use Adianti\Widget\Base\TStyle;
 use Exception;
 
 class TFancytree extends TElement
@@ -14,18 +15,13 @@ class TFancytree extends TElement
     private $source;
     private $height;
     private $width;
-    private $autoCollapse;
     private $checkbox;
     private $selectMode;
-    private $editable;
     private $dragDrop;
-    private $clickAction;
     private $onClickAction;
     private $onSelectAction;
-    private $onActivateAction;
     private $selectedKeys;
     private $expandedKeys;
-    private static $scriptsIncluded = false;
 
     /**
      * Constructor
@@ -36,28 +32,6 @@ class TFancytree extends TElement
         $this->id = 'tfancytree_' . mt_rand(1000000000, 1999999999);
         $this->{'id'} = $this->id;
         $this->selectMode = 2;
-        $this->clickAction = 'activate';
-
-        // Registrar assets do componente
-        $this->registerAssets();
-    }
-
-    /**
-     * Register required assets
-     */
-    private function registerAssets()
-    {
-        if (!self::$scriptsIncluded) {
-            // Registrar CSS e JS do componente
-            parent::addStyle('fancytree-css', 'vendor/marcelonees/tfancytree/assets/css/ui.fancytree.min.css');
-            parent::addScript('fancytree-js', 'vendor/marcelonees/tfancytree/assets/js/jquery.fancytree-all-deps.min.js');
-
-            // jQuery UI (dependência)
-            parent::addStyle('jquery-ui', 'https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css');
-            parent::addScript('jquery-ui', 'https://code.jquery.com/ui/1.13.2/jquery-ui.min.js');
-
-            self::$scriptsIncluded = true;
-        }
     }
 
     /**
@@ -66,6 +40,15 @@ class TFancytree extends TElement
     public function setSource($source)
     {
         $this->source = $source;
+    }
+
+    /**
+     * Set size
+     */
+    public function setSize($width, $height)
+    {
+        $this->setWidth($width);
+        $this->setHeight($height);
     }
 
     /**
@@ -151,14 +134,6 @@ class TFancytree extends TElement
     }
 
     /**
-     * Set onActivate action
-     */
-    public function setOnActivate(TAction $action)
-    {
-        $this->onActivateAction = $action;
-    }
-
-    /**
      * Build node from array
      */
     private function buildNode($node)
@@ -166,9 +141,9 @@ class TFancytree extends TElement
         $fancyNode = [];
 
         if (isset($node['id'])) {
-            $fancyNode['key'] = $node['id'];
+            $fancyNode['key'] = (string) $node['id'];
         } elseif (isset($node['key'])) {
-            $fancyNode['key'] = $node['key'];
+            $fancyNode['key'] = (string) $node['key'];
         }
 
         if (isset($node['text'])) {
@@ -186,7 +161,141 @@ class TFancytree extends TElement
             $fancyNode['lazy'] = true;
         }
 
+        if (isset($node['expanded']) && $node['expanded']) {
+            $fancyNode['expanded'] = true;
+        }
+
+        if (isset($node['selected']) && $node['selected']) {
+            $fancyNode['selected'] = true;
+        }
+
+        if (isset($node['icon'])) {
+            $fancyNode['icon'] = $node['icon'];
+        }
+
+        if (isset($node['folder'])) {
+            $fancyNode['folder'] = $node['folder'];
+        }
+
         return $fancyNode;
+    }
+
+    /**
+     * Create the tree (similar to createMap)
+     */
+    public function createTree()
+    {
+        // Verifica se os arquivos necessários existem
+        $requiredFiles = [
+            'vendor/marcelonees/tfancytree/assets/jquery-ui/css/jquery-ui.css',
+            'vendor/marcelonees/tfancytree/assets/jquery-ui/js/jquery-ui.min.js',
+            'vendor/marcelonees/tfancytree/assets/fancytree/css/ui.fancytree.min.css',
+            'vendor/marcelonees/tfancytree/assets/fancytree/js/jquery.fancytree-all-deps.min.js'
+        ];
+
+        foreach ($requiredFiles as $file) {
+            if (!file_exists($file)) {
+                throw new Exception("Arquivo necessário não encontrado: {$file}");
+            }
+        }
+
+        // Processar source
+        $processedSource = [];
+        foreach ($this->source as $node) {
+            $processedSource[] = $this->buildNode($node);
+        }
+        $sourceJson = json_encode($processedSource);
+
+        // Preparar configuração
+        $config = [
+            'source' => $processedSource,
+            'checkbox' => $this->checkbox === true,
+            'selectMode' => (int) $this->selectMode
+        ];
+
+        if ($this->dragDrop) {
+            $config['dnd'] = ['autoExpandMS' => 1000];
+        }
+
+        if ($this->selectedKeys && is_array($this->selectedKeys)) {
+            $config['selectedKeys'] = $this->selectedKeys;
+        }
+
+        if ($this->expandedKeys && is_array($this->expandedKeys)) {
+            $config['expandedKeys'] = $this->expandedKeys;
+        }
+
+        $configJson = json_encode($config);
+
+        // Preparar eventos
+        $events = [];
+        if ($this->onClickAction) {
+            $serializedAction = $this->onClickAction->serialize(false);
+            $events[] = "click: function(event, data) { __adianti_ajax_exec('{$serializedAction}&key=' + encodeURIComponent(data.node.key)); }";
+        }
+
+        if ($this->onSelectAction) {
+            $serializedAction = $this->onSelectAction->serialize(false);
+            $events[] = "select: function(event, data) { var keys = data.tree.getSelectedKeys(); __adianti_ajax_exec('{$serializedAction}&keys=' + JSON.stringify(keys)); }";
+        }
+
+        $eventsStr = !empty($events) ? ', ' . implode(', ', $events) : '';
+
+        // Garante que o CSS seja carregado primeiro
+        TStyle::importFromFile('vendor/marcelonees/tfancytree/assets/jquery-ui/css/jquery-ui.css');
+        TStyle::importFromFile('vendor/marcelonees/tfancytree/assets/fancytree/css/ui.fancytree.min.css');
+
+        // Cria o sistema de carregamento robusto
+        TScript::create("
+        /* Função principal de inicialização */
+        function initializeTree_{$this->id}() {
+            try {
+                $('#{$this->id}').fancytree({$configJson}{$eventsStr});
+                console.log('Fancytree inicializado com sucesso!');
+            } catch(initError) {
+                console.error('Erro na inicialização da árvore:', initError);
+                $('#{$this->id}').html('<div style=\"padding:20px;color:red;\">Erro ao carregar a árvore</div>');
+            }
+        }
+        
+        /* Carrega os scripts necessários em ordem */
+        var requiredScripts = [
+            'vendor/marcelonees/tfancytree/assets/jquery-ui/js/jquery-ui.min.js',
+            'vendor/marcelonees/tfancytree/assets/fancytree/js/jquery.fancytree-all-deps.min.js'
+        ];
+        
+        /* Função para carregar scripts sequencialmente */
+        function loadScript(scripts, callback) {
+            if (scripts.length === 0) {
+                callback();
+                return;
+            }
+            
+            var currentScript = scripts.shift();
+            $.getScript(currentScript)
+                .done(function() {
+                    console.log('Script carregado:', currentScript);
+                    loadScript(scripts, callback);
+                })
+                .fail(function() {
+                    console.error('Falha ao carregar:', currentScript);
+                    loadScript(scripts, callback);
+                });
+        }
+        
+        /* Inicia o processo de carregamento */
+        loadScript(requiredScripts, function() {
+            /* Verifica se todos os requisitos estão carregados */
+            if (typeof $.fn.fancytree === 'undefined') {
+                console.error('Fancytree não carregado corretamente');
+                $('#{$this->id}').html('<div style=\"padding:20px;color:red;\">Biblioteca Fancytree não carregada</div>');
+                return;
+            }
+            
+            /* Executa a inicialização */
+            setTimeout(initializeTree_{$this->id}, 100);
+        });
+        ");
     }
 
     /**
@@ -198,55 +307,14 @@ class TFancytree extends TElement
             throw new Exception(AdiantiCoreTranslator::translate('You must call ^1 before ^2', __METHOD__ . '::setSource', 'show'));
         }
 
-        $config = [
-            'checkbox' => $this->checkbox,
-            'selectMode' => $this->selectMode,
-            'clickAction' => $this->clickAction
-        ];
+        // Adiciona estilo à div
+        $currentStyle = isset($this->{'style'}) ? $this->{'style'} : '';
+        $this->{'style'} = $currentStyle . ';border:1px solid #ddd;background:#fff;';
 
-        if ($this->dragDrop) {
-            $config['dnd'] = ['autoExpandMS' => 1000];
-        }
-
-        if ($this->selectedKeys) {
-            $config['selectedKeys'] = $this->selectedKeys;
-        }
-
-        if ($this->expandedKeys) {
-            $config['expandedKeys'] = $this->expandedKeys;
-        }
-
-        $events = [];
-
-        if ($this->onClickAction) {
-            $serializedAction = $this->onClickAction->serialize(false);
-            $events[] = "click: function(event, data) { __adianti_ajax_exec('{$serializedAction}&key=' + data.node.key); }";
-        }
-
-        if ($this->onSelectAction) {
-            $serializedAction = $this->onSelectAction->serialize(false);
-            $events[] = "select: function(event, data) { var keys = data.tree.getSelectedKeys(); __adianti_ajax_exec('{$serializedAction}&keys=' + JSON.stringify(keys)); }";
-        }
-
-        $sourceScript = "source: " . json_encode(array_map([$this, 'buildNode'], $this->source));
-        $configScript = json_encode($config);
-        $eventsScript = !empty($events) ? ", " . implode(", ", $events) : "";
-
-        $script = new TElement('script');
-        $script->{'type'} = 'text/javascript';
-        $script->add(
-            <<<JS
-        $(function() {
-            $("#{$this->id}").fancytree({
-                {$sourceScript},
-                {$configScript}
-                {$eventsScript}
-            });
-        });
-JS
-        );
-
-        parent::add($script);
+        // Mostra a div
         parent::show();
+
+        // Cria a árvore
+        $this->createTree();
     }
 }
